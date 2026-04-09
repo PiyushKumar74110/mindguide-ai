ac2c9dd14cb7d5ccf471a481567994de305e4783
PiyushKumar74110 <piyushkumar74110@gmail.com>
2026-04-09 23:41:52 +0530

    Update confidence.py

--- src/confidence/confidence.py
+++ src/confidence/confidence.py
@@ -1,1 +1,15 @@
+import numpy as np
 
+def get_classification_confidence(proba):
+    return np.ax(proba, axis=1)
+
+def get_uncertainity_flag(confidence, threshold=0.6):
+    return (confidence<threshold).astype(int)
+
+def get_regression_confidence(predictions):
+    conf  = 1 - (np.abs(predictions-3)/2)
+
+    return np.clip(conf,0,1)
+
+def combine_confidence(class_conf, reg_conf, alpha=0.7):
+    return alpha*class_conf + (1-alpha)*reg_conf
