caa6641c6b7c4bf7c6bd1797846a208b1fc9c019
PiyushKumar74110 <piyushkumar74110@gmail.com>
2026-04-10 00:12:16 +0530

    Update train_intensity_model.py

--- src/model/train_intensity_model.py
+++ src/model/train_intensity_model.py
@@ -1,1 +1,13 @@
+from xgboost import XGBRegressor
 
+def train_intensity_model(X_train, y_train):
+    model = XGBRegressor(
+        max_depth=4,
+        n_estimators=150,
+        learning_rate=0.1,
+        random_state=42
+    )
+
+    model.fit(X_train,y_train)
+    return model
+    
