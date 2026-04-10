f54fe2a22955265b874051b6074bf2bd444f668a
PiyushKumar74110 <piyushkumar74110@gmail.com>
2026-04-10 18:01:56 +0530

    Update train_state_model.py

--- src/model/train_state_model.py
+++ src/model/train_state_model.py
@@ -1,14 +1,12 @@
 from xgboost import XGBClassifier
 
-def train_state_model(X_train, y_train):
+def train_state_model(X, y):
     model = XGBClassifier(
-        max_depth=4,
-        n_estimators=150,
-        learning_rate = 0.1,
-        use_label_encoder = False,
-        eval_METRIC = 'mlogloss',
-        random_state = 42
+        n_estimators=100,
+        max_depth=5,
+        learning_rate=0.1,
+        # use_label_encoder=False,
+        eval_metric='mlogloss'
     )
-
-    model.fit(X_train, y_train)
+    model.fit(X, y)
     return model
