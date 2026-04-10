b45a2b645270898f0735d64349cae3ce3386d467
PiyushKumar74110 <piyushkumar74110@gmail.com>
2026-04-10 18:03:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
+import argparse
+import subprocess
 from src.pipeline.run_pipeline import run_pipeline
 
+def main(mode):
 
-if __name__=="__main__":
-    train_path = "data/sample_data/sample_data.csv"
-    test_path = "data/test_data/test_data.csv"
-    output_path = "outputs/predictions/test_predictions.csv"
+    if mode == "train":
+        run_pipeline(
+            "data/sample_data/sample_data.csv",
+            "data/test_data/test_data.csv",
+            "outputs/predictions/test_predictions.csv"
+        )
 
-    run_pipeline(train_path, test_path, output_path)
+    elif mode == "app":
+        subprocess.run(["streamlit", "run", "app/streamlit_app.py"])
+
+    elif mode == "all":
+        run_pipeline(
+            "data/sample_data/sample_data.csv",
+            "data/test_data/test_data.csv",
+            "outputs/predictions/test_predictions.csv"
+        )
+        subprocess.run(["streamlit", "run", "app/streamlit_app.py"])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", default="train")
+    args = parser.parse_args()
+    main(args.mode)
+    
+    
+    
+
+    
