d4f9b7d823df3c000c97eafc807db57da1012b40
PiyushKumar74110 <piyushkumar74110@gmail.com>
2026-04-10 17:59:12 +0530

    Update decision_engine.py

--- src/decision/decision_engine.py
+++ src/decision/decision_engine.py
@@ -1,29 +1,21 @@
-def decision_action(pred_state, intensity, stress, energy, time_of_day):
-    action = "pause"
-    when = "now"
+def decide_action(pred_state, intensity, stress, energy, time_of_day):
 
-    if pred_state in ['stressed', 'anxious']:
-        if intensity>=4:
-            action = 'rest' if energy<=2 else 'box_breathing'
+    if pred_state in ["stressed","anxious"]:
+        if intensity >= 4:
+            return "box_breathing","now"
         else:
-            action = 'journaling'
-    elif pred_state in ['calm','relaxed']:
-        action = 'deep_work' if energy>=4 else 'light_planning'
+            return "journaling","within_15_min"
 
-    if stress>=4 and energy<=2:
-        action = 'rest'
-    elif stress<=2 and energy >=4:
-        action = 'movement'
+    elif pred_state == "sad":
+        if energy <= 2:
+            return "rest","now"
+        else:
+            return "movement","within_15_min"
 
-    if action in ['rest', 'yoga', 'sound_therapy']:
-        when = 'tonight' if time_of_day in [2,3] else 'later_today'
-    elif action in ['deep_work', 'journaling', 'movement']:
-        when = 'now' if time_of_day in [0,1] else 'later_today'
-    else:
-        when = 'within_15_min'
+    elif pred_state in ["calm","relaxed"]:
+        if energy >= 4:
+            return "deep_work","now"
+        else:
+            return "light_planning","later_today"
 
-    return action,when
-
-
-    
-
+    return "pause","now"
